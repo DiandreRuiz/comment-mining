a2fc57104f428b39bfea04d6cdee4f58d5590aad
DiandreRuiz <Diandreruiz77@gmail.com>
2025-10-29 17:00:31 -0400

    Added official javascript client library from youtube

--- src/services/googleApiYoutube.tsx
+++ src/services/googleApiYoutube.tsx
@@ -1,3 +1,10 @@
+import { google } from "googleapis";
+
+const YOUTUBE_API_CLIENT = google.youtube({
+    version: "v3",
+    auth: import.meta.env.VITE_YT_API_KEY,
+});
+
 // services/youtubeService.ts
 export async function searchYouTubeChannels(query: string, signal?: AbortSignal) {
     const params = new URLSearchParams({
@@ -12,14 +19,12 @@ export async function searchYouTubeChannels(query: string, signal?: AbortSignal)
     return res.json();
 }
 
-export async function getYoutubeChannelComments(query: string, signal?: AbortSignal) {
-    const params = new URLSearchParams({
-        part: "snippet",
-        type: "comment",
-        q: query,
-        key: import.meta.env.VITE_YT_API_KEY,
+export async function getYoutubeChannelComments(videoId: string) {
+    const res = await YOUTUBE_API_CLIENT.commentThreads.list({
+        params: {
+            part: "snippet",
+            videoId,
+        },
     });
-    const res = await fetch(`https://www.googleapis.com/youtube/v3/comments?${params}`, { signal });
-    if (!res.ok) throw new Error("Failed to fetch channels");
-    return res.json();
+    return res.data;
 }
