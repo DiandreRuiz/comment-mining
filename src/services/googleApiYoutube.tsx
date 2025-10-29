1affda1cbf8f3d971e301157105de39f455fb41b
DiandreRuiz <Diandreruiz77@gmail.com>
2025-10-29 17:44:57 -0400

    Sucessully implement YouTube api request for top 3 videos from a channelId using URLSearchParams and fetch

--- src/services/googleApiYoutube.tsx
+++ src/services/googleApiYoutube.tsx
@@ -12,6 +12,17 @@ export async function searchYouTubeChannels(query: string, signal?: AbortSignal)
     return res.json();
 }
 
-export async function getTop3VideosByViews(channelId: string) {
-    
+export async function getTop3VideosByViews(channelId: string, signal?: AbortSignal) {
+    const params = new URLSearchParams({
+        part: "id",
+        type: "video",
+        channelId: channelId.toString(),
+        order: "viewCount",
+        maxResults: "3",
+        key: import.meta.env.VITE_YT_API_KEY,
+    });
+
+    const res = await fetch(`https://www.googleapis.com/youtube/v3/search?${params}`, { signal });
+    if (!res.ok) throw new Error("Failed to fetch videos");
+    return res.json();
 }
