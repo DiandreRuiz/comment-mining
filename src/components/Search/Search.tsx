02448ee7c9827ae96eb7e6791f6196401c425f02
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-18 14:33:38 -0500

    Fixed bug with margin being accidentally added to the wrong area resulting in smart search dropdown being detached from search bar

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -28,60 +28,62 @@ const Search: React.FC = () => {
     return (
         <div style={{ position: "relative" }}>
             <h1>YouTube Channel Search</h1>
-            <div className="d-flex justify-content-center mb-5">
-                <InputGroup style={{ width: "50%" }}>
-                    <Form.Control
-                        type="text"
-                        placeholder="Search..."
-                        value={query}
-                        onChange={(e) => {
-                            const val = e.target.value;
-                            setQuery(val);
-                            setShowOptions(val.length > 2);
-                        }}
-                        style={{ boxShadow: "none" }}
-                        onFocus={(e) => (e.target.style.boxShadow = "none")}
-                        onBlur={(e) => (e.target.style.boxShadow = "none")}
-                    />
-                    <Button type="submit" variant="primary">
-                        Research
-                    </Button>
-                </InputGroup>
-            </div>
+            <div className="mb-5">
+                <div className="d-flex justify-content-center">
+                    <InputGroup style={{ width: "50%" }}>
+                        <Form.Control
+                            type="text"
+                            placeholder="Search..."
+                            value={query}
+                            onChange={(e) => {
+                                const val = e.target.value;
+                                setQuery(val);
+                                setShowOptions(val.length > 2);
+                            }}
+                            style={{ boxShadow: "none" }}
+                            onFocus={(e) => (e.target.style.boxShadow = "none")}
+                            onBlur={(e) => (e.target.style.boxShadow = "none")}
+                        />
+                        <Button type="submit" variant="primary">
+                            Research
+                        </Button>
+                    </InputGroup>
+                </div>
 
-            {
-                // Checks if the user's query is at least 1 character long and
-                // that there is at least 1 result. Need to add form submission
-                // functionality and automation upon choosing an option
-            }
-            <Dropdown show={showOptions} className="mx-auto w-50">
-                <Dropdown.Menu>
-                    {isLoading ? (
-                        <div className="text-center p-2">
-                            <Spinner />
-                        </div>
-                    ) : error ? (
-                        <div className="px-3 py-2">Error: {error}</div>
-                    ) : results.length > 0 ? (
-                        results.map((item) => (
-                            <Dropdown.Item
-                                key={item.id.channelId}
-                                onClick={() => {
-                                    setSelectedChannelId(item.id.channelId ?? null);
-                                    setShowOptions(false);
-                                    setQuery(item.snippet.title);
-                                }}
-                            >
-                                {item.snippet.title}
-                            </Dropdown.Item>
-                        ))
-                    ) : (
-                        <div className="text-center p-2">
-                            <Spinner />
-                        </div>
-                    )}
-                </Dropdown.Menu>
-            </Dropdown>
+                {
+                    // Checks if the user's query is at least 1 character long and
+                    // that there is at least 1 result. Need to add form submission
+                    // functionality and automation upon choosing an option
+                }
+                <Dropdown show={showOptions} className="mx-auto w-50">
+                    <Dropdown.Menu>
+                        {isLoading ? (
+                            <div className="text-center p-2">
+                                <Spinner />
+                            </div>
+                        ) : error ? (
+                            <div className="px-3 py-2">Error: {error}</div>
+                        ) : results.length > 0 ? (
+                            results.map((item) => (
+                                <Dropdown.Item
+                                    key={item.id.channelId}
+                                    onClick={() => {
+                                        setSelectedChannelId(item.id.channelId ?? null);
+                                        setShowOptions(false);
+                                        setQuery(item.snippet.title);
+                                    }}
+                                >
+                                    {item.snippet.title}
+                                </Dropdown.Item>
+                            ))
+                        ) : (
+                            <div className="text-center p-2">
+                                <Spinner />
+                            </div>
+                        )}
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
             {selectedChannelId && <SummaryBox channelId={selectedChannelId} />}
         </div>
     );
