d7a612bc06aad9c164c9999103eda74073923e7d
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-18 10:56:24 -0500

    Fixed issue with missing catch / finally in services openai implementation

--- src/services/openai.ts
+++ src/services/openai.ts
@@ -1,12 +1,7 @@
-import axios from "axios";
-
 export async function analyzeComments(commentsByVideoId: Record<string, string[]>) {
     let allComments: string[] = [];
     for (const videoId in commentsByVideoId) {
         const currentVideosComments = commentsByVideoId[videoId];
         allComments = [...allComments, ...currentVideosComments];
-    }
-    try {
-        const response = axios.get()
     }
 }
